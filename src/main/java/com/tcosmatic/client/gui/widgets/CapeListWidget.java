26d5d86f6b65d4420709cf08f4537bc97cefb63c
Twicefear <ytd82774@gmail.com>
2026-01-01 18:34:28 +0530

    AI Auto-Commit: Cape list widget.

---
+++ src/main/java/com/tcosmatic/client/gui/widgets/CapeListWidget.java
@@ -0,0 +1,8 @@
+package com.tcosmatic.client.gui.widgets;
+
+import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.text.Text;
+
+public class CapeListWidget {
+    // Implementation for list widget
+}
