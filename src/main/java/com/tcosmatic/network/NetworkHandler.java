96a10913d3e1d19849b5e9f2bba5d61c98aac40c
Twicefear <ytd82774@gmail.com>
2026-01-01 20:01:15 +0530

    Update NetworkHandler.java

--- src/main/java/com/tcosmatic/network/NetworkHandler.java
+++ src/main/java/com/tcosmatic/network/NetworkHandler.java
@@ -1,38 +1,41 @@
 package com.tcosmatic.network;
 
 import com.tcosmatic.TCosmaticMod;
+import com.tcosmatic.util.CapeManager;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.util.Identifier;
-import net.minecraft.client.MinecraftClient;
+import net.minecraft.util.registry.Registry;
 
 public class NetworkHandler {
 
     public static final Identifier CAPE_SYNC_ID = new Identifier(TCosmaticMod.MOD_ID, "cape_sync");
 
     public static void registerPackets() {
-        // Client-side packet registration
         ClientPlayNetworking.registerGlobalReceiver(CAPE_SYNC_ID, (client, handler, buf, responseSender) -> {
             String playerUuid = buf.readString();
             String capeName = buf.readString();
 
             client.execute(() -> {
                 TCosmaticMod.LOGGER.info("Received cape sync for player: " + playerUuid + " - Cape: " + capeName);
-                // TODO: Store playerUuid -> capeName mapping for rendering other players
+                // Update CapeManager map for other players
+                CapeManager.getInstance().setOtherPlayerCape(playerUuid, capeName);
             });
         });
     }
 
     public static void sendCapeSync(String capeName) {
-        if (!ClientPlayNetworking.canSend(CAPE_SYNC_ID)) return;
-
         MinecraftClient client = MinecraftClient.getInstance();
+        ClientPlayerEntity player = client.player;
+        if (player == null) return;
 
-        PacketByteBuf buf = new PacketByteBuf(PacketByteBufs.create());
-        buf.writeString(client.player.getUuidAsString());
+        PacketByteBuf buf = new PacketByteBuf(net.minecraft.network.PacketByteBufs.create());
+        buf.writeString(player.getUuidAsString());
         buf.writeString(capeName);
 
         ClientPlayNetworking.send(CAPE_SYNC_ID, buf);
-        TCosmaticMod.LOGGER.info("Sent cape sync: " + capeName);
+        TCosmaticMod.LOGGER.info("Sending cape sync: " + capeName);
     }
 }
