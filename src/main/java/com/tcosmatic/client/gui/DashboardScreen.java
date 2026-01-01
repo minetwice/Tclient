564162027cb3e51360fa68de988ab74f6d17e9ee
Twicefear <ytd82774@gmail.com>
2026-01-01 18:34:26 +0530

    AI Auto-Commit: Dashboard screen.

---
+++ src/main/java/com/tcosmatic/client/gui/DashboardScreen.java
@@ -0,0 +1,14 @@
+package com.tcosmatic.client.gui;
+
+import net.minecraft.text.Text;
+
+public class DashboardScreen extends TCosmaticScreen {
+    public DashboardScreen() {
+        super(Text.literal("TCosmatic Dashboard"));
+    }
+    
+    @Override
+    protected void init() {
+        super.init();
+    }
+}
