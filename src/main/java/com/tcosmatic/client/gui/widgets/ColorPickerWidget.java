078cb9a7f13d108502a8570ab359bf360c1e55e0
Twicefear <ytd82774@gmail.com>
2026-01-01 19:52:04 +0530

    Update ColorPickerWidget.java

--- src/main/java/com/tcosmatic/client/gui/widgets/ColorPickerWidget.java
+++ src/main/java/com/tcosmatic/client/gui/widgets/ColorPickerWidget.java
@@ -1,6 +1,5 @@
 package com.tcosmatic.client.gui.widgets;
 
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.Drawable;
 import net.minecraft.client.gui.Element;
@@ -16,7 +15,6 @@ public class ColorPickerWidget implements Drawable, Element, Selectable {
     private final Consumer<Integer> onColorChange;
     private int selectedColor = 0xFFFFFFFF;
 
-    // Predefined color palette
     private static final int[] COLORS = {
             0xFFFFFFFF, 0xFFCCCCCC, 0xFF999999, 0xFF666666, 0xFF333333, 0xFF000000,
             0xFFFF0000, 0xFFFF8800, 0xFFFFFF00, 0xFF00FF00, 0xFF00FFFF, 0xFF0000FF,
@@ -37,22 +35,11 @@ public class ColorPickerWidget implements Drawable, Element, Selectable {
 
     @Override
     public void render(DrawContext context, int mouseX, int mouseY, float delta) {
-        MinecraftClient client = MinecraftClient.getInstance();
-
-        // Background
         context.fill(x, y, x + width, y + height, 0x80000000);
         context.drawBorder(x, y, width, height, 0xFF555555);
 
-        // Title (FIXED for 1.21.1)
-        context.drawTextWithShadow(
-                client.textRenderer,
-                "Color Picker",
-                x + 10,
-                y + 10,
-                0xFFFFFF
-        );
+        context.drawTextWithShadow(context.getMatrices().peek().getPositionMatrix(), "Color Picker", x + 10, y + 10, 0xFFFFFF);
 
-        // Draw color palette
         int startY = y + 30;
         for (int i = 0; i < COLORS.length; i++) {
             int col = i % COLORS_PER_ROW;
@@ -61,50 +48,15 @@ public class ColorPickerWidget implements Drawable, Element, Selectable {
             int colorX = x + 10 + col * (COLOR_SIZE + 5);
             int colorY = startY + row * (COLOR_SIZE + 5);
 
-            // Color swatch
             context.fill(colorX, colorY, colorX + COLOR_SIZE, colorY + COLOR_SIZE, COLORS[i]);
 
-            // Selected border
-            if (COLORS[i] == selectedColor) {
-                context.drawBorder(
-                        colorX - 2,
-                        colorY - 2,
-                        COLOR_SIZE + 4,
-                        COLOR_SIZE + 4,
-                        0xFFFFFFFF
-                );
-            }
-
-            // Hover border
-            if (isMouseOver(colorX, colorY, COLOR_SIZE, COLOR_SIZE, mouseX, mouseY)) {
-                context.drawBorder(
-                        colorX - 1,
-                        colorY - 1,
-                        COLOR_SIZE + 2,
-                        COLOR_SIZE + 2,
-                        0xFF888888
-                );
-            }
+            if (COLORS[i] == selectedColor) context.drawBorder(colorX - 2, colorY - 2, COLOR_SIZE + 4, COLOR_SIZE + 4, 0xFFFFFFFF);
+            if (isMouseOver(colorX, colorY, COLOR_SIZE, COLOR_SIZE, mouseX, mouseY)) context.drawBorder(colorX - 1, colorY - 1, COLOR_SIZE + 2, COLOR_SIZE + 2, 0xFF888888);
         }
 
-        // Current color label
         int currentColorY = startY + ((COLORS.length / COLORS_PER_ROW) + 1) * (COLOR_SIZE + 5);
-        context.drawTextWithShadow(
-                client.textRenderer,
-                "Current:",
-                x + 10,
-                currentColorY,
-                0xFFFFFF
-        );
-
-        // Current color preview
-        context.fill(
-                x + 70,
-                currentColorY - 2,
-                x + 120,
-                currentColorY + 18,
-                selectedColor
-        );
+        context.drawTextWithShadow(context.getMatrices().peek().getPositionMatrix(), "Current:", x + 10, currentColorY, 0xFFFFFF);
+        context.fill(x + 70, currentColorY - 2, x + 120, currentColorY + 18, selectedColor);
         context.drawBorder(x + 70, currentColorY - 2, 50, 20, 0xFFFFFFFF);
     }
 
@@ -115,7 +67,6 @@ public class ColorPickerWidget implements Drawable, Element, Selectable {
         for (int i = 0; i < COLORS.length; i++) {
             int col = i % COLORS_PER_ROW;
             int row = i / COLORS_PER_ROW;
-
             int colorX = x + 10 + col * (COLOR_SIZE + 5);
             int colorY = startY + row * (COLOR_SIZE + 5);
 
@@ -147,11 +98,7 @@ public class ColorPickerWidget implements Drawable, Element, Selectable {
     }
 
     @Override
-    public void setFocused(boolean focused) {
-    }
-
+    public void setFocused(boolean focused) {}
     @Override
-    public boolean isFocused() {
-        return false;
-    }
+    public boolean isFocused() { return false; }
 }
