27d0fd4bd73f3ac457cf864605ae1ff11195dcad
Twicefear <ytd82774@gmail.com>
2026-01-01 19:53:06 +0530

    Update CapeListWidget.java

--- src/main/java/com/tcosmatic/client/gui/widgets/CapeListWidget.java
+++ src/main/java/com/tcosmatic/client/gui/widgets/CapeListWidget.java
@@ -2,7 +2,6 @@ package com.tcosmatic.client.gui.widgets;
 
 import com.tcosmatic.util.CapeManager;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.widget.AlwaysSelectedEntryListWidget;
 import net.minecraft.text.Text;
@@ -12,66 +11,53 @@ public class CapeListWidget extends AlwaysSelectedEntryListWidget<CapeListWidget
     private CapeManager.CapeData selectedCape;
 
     public CapeListWidget(MinecraftClient client, int width, int height, int top, int bottom) {
-        super(client, width, height, top, bottom, 32);
+        super(client, width, height, top, bottom, 32); // 1.21.1 uses 5-arg constructor
     }
 
     public void addCape(CapeManager.CapeData cape) {
-        addEntry(new CapeEntry(cape));
+        this.addEntry(new CapeEntry(cape));
     }
 
-    public CapeManager.CapeData getSelectedCape() {
-        return selectedCape;
+    public void setSelectedCape(CapeManager.CapeData cape) {
+        this.selectedCape = cape;
+        if (cape != null) {
+            for (CapeEntry entry : this.children()) {
+                if (entry.getCape() == cape) this.setSelected(entry);
+            }
+        }
     }
 
-    public class CapeEntry extends AlwaysSelectedEntryListWidget.Entry<CapeEntry> {
+    public CapeManager.CapeData getSelectedCape() { return selectedCape; }
 
+    public class CapeEntry extends Entry<CapeEntry> {
         private final CapeManager.CapeData cape;
 
-        public CapeEntry(CapeManager.CapeData cape) {
-            this.cape = cape;
-        }
+        public CapeEntry(CapeManager.CapeData cape) { this.cape = cape; }
+
+        public CapeManager.CapeData getCape() { return cape; }
 
         @Override
-        public void render(DrawContext context, int index, int y, int x, int w, int h,
-                           int mouseX, int mouseY, boolean hovered, float delta) {
+        public void render(DrawContext context, int index, int y, int x, int entryWidth, int entryHeight,
+                           int mouseX, int mouseY, boolean hovered, float tickDelta) {
+            MinecraftClient client = MinecraftClient.getInstance();
+            if (cape == selectedCape) context.fill(x, y, x + entryWidth, y + entryHeight, 0x80FFFFFF);
+            else if (hovered) context.fill(x, y, x + entryWidth, y + entryHeight, 0x40FFFFFF);
 
-            TextRenderer tr = MinecraftClient.getInstance().textRenderer;
+            if (cape.getTextureId() != null) context.drawTexture(cape.getTextureId(), x + 2, y + 2, 0, 0, 28, 28, 64, 32);
 
-            if (cape == selectedCape) {
-                context.fill(x, y, x + w, y + h, 0x803F51B5);
-            } else if (hovered) {
-                context.fill(x, y, x + w, y + h, 0x40FFFFFF);
-            }
-
-            if (cape.getTextureId() != null) {
-                context.drawTexture(
-                        cape.getTextureId(),
-                        x + 4, y + 2,
-                        0, 0,
-                        24, 16,
-                        64, 32
-                );
-            }
-
-            context.drawTextWithShadow(
-                    tr,
-                    Text.literal(cape.getName()),
-                    x + 36,
-                    y + 10,
-                    0xFFFFFF
-            );
+            context.drawTextWithShadow(client.textRenderer, cape.getName(), x + 35, y + 10, 0xFFFFFF);
         }
 
         @Override
         public boolean mouseClicked(double mouseX, double mouseY, int button) {
-            selectedCape = cape;
-            setSelected(this);
+            selectedCape = this.cape;
+            CapeListWidget.this.setSelected(this);
             return true;
         }
 
         @Override
         public Text getNarration() {
-            return Text.literal("Cape " + cape.getName());
+            return Text.literal("Cape: " + cape.getName());
         }
     }
 }
