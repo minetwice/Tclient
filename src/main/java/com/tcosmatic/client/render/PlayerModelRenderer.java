3146dbb0beda18eaf6704a9e1e977aa4e91d744a
Twicefear <ytd82774@gmail.com>
2026-01-01 20:01:50 +0530

    Update PlayerModelRenderer.java

--- src/main/java/com/tcosmatic/client/render/PlayerModelRenderer.java
+++ src/main/java/com/tcosmatic/client/render/PlayerModelRenderer.java
@@ -13,10 +13,8 @@ import org.joml.Quaternionf;
 
 public class PlayerModelRenderer {
 
-    private final MinecraftClient client;
-
-    public PlayerModelRenderer() { // 1.21.1 version uses no-arg constructor
-        this.client = MinecraftClient.getInstance();
+    public PlayerModelRenderer() {
+        // 1.21.1 compatible no-arg constructor
     }
 
     public void renderPlayerWithCape(DrawContext context, int x, int y, int size,
@@ -32,16 +30,16 @@ public class PlayerModelRenderer {
         matrices.scale(size, size, -size);
         matrices.multiply(RotationAxis.POSITIVE_Z.rotationDegrees(180.0F));
 
-        Quaternionf xRot = RotationAxis.POSITIVE_X.rotationDegrees(mouseY * 0.2F);
-        Quaternionf yRot = RotationAxis.POSITIVE_Y.rotationDegrees(mouseX * 0.2F);
-        matrices.multiply(xRot);
-        matrices.multiply(yRot);
+        Quaternionf rotationX = RotationAxis.POSITIVE_X.rotationDegrees(mouseY * 0.2F);
+        matrices.multiply(rotationX);
+        Quaternionf rotationY = RotationAxis.POSITIVE_Y.rotationDegrees(mouseX * 0.2F);
+        matrices.multiply(rotationY);
 
-        EntityRenderDispatcher dispatcher = client.getEntityRenderDispatcher();
+        EntityRenderDispatcher dispatcher = MinecraftClient.getInstance().getEntityRenderDispatcher();
         dispatcher.setRenderShadows(false);
 
-        VertexConsumerProvider.Immediate immediate = client.getBufferBuilders().getEntityVertexConsumers();
-        dispatcher.render(player, 0, 0, 0, 0, 1, matrices, immediate, 15728880);
+        VertexConsumerProvider.Immediate immediate = MinecraftClient.getInstance().getBufferBuilders().getEntityVertexConsumers();
+        dispatcher.render(player, 0,0,0,0,1.0F, matrices, immediate, 15728880);
         immediate.draw();
         dispatcher.setRenderShadows(true);
 
@@ -49,23 +47,26 @@ public class PlayerModelRenderer {
     }
 
     public void renderEntity(DrawContext context, int x, int y, int size,
-                             float mouseX, float mouseY,
-                             LivingEntity entity) {
+                             float mouseX, float mouseY, LivingEntity entity) {
         if (entity == null) return;
 
         MatrixStack matrices = context.getMatrices();
         matrices.push();
+
         matrices.translate(x, y, 50);
         matrices.scale(size, size, -size);
         matrices.multiply(RotationAxis.POSITIVE_Z.rotationDegrees(180.0F));
-        matrices.multiply(RotationAxis.POSITIVE_X.rotationDegrees(mouseY * 0.3F));
-        matrices.multiply(RotationAxis.POSITIVE_Y.rotationDegrees(mouseX * 0.3F));
 
-        EntityRenderDispatcher dispatcher = client.getEntityRenderDispatcher();
+        Quaternionf xRotation = RotationAxis.POSITIVE_X.rotationDegrees(mouseY * 0.3F);
+        matrices.multiply(xRotation);
+        Quaternionf yRotation = RotationAxis.POSITIVE_Y.rotationDegrees(mouseX * 0.3F);
+        matrices.multiply(yRotation);
+
+        EntityRenderDispatcher dispatcher = MinecraftClient.getInstance().getEntityRenderDispatcher();
         dispatcher.setRenderShadows(false);
 
-        VertexConsumerProvider.Immediate immediate = client.getBufferBuilders().getEntityVertexConsumers();
-        dispatcher.render(entity, 0, 0, 0, 0, 1, matrices, immediate, 15728880);
+        VertexConsumerProvider.Immediate immediate = MinecraftClient.getInstance().getBufferBuilders().getEntityVertexConsumers();
+        dispatcher.render(entity,0,0,0,0,1.0F, matrices, immediate, 15728880);
         immediate.draw();
         dispatcher.setRenderShadows(true);
 
