9a8edfa0d6074bd2c559954f4c487fb01e45fe2d
Twicefear <ytd82774@gmail.com>
2026-01-01 18:34:25 +0530

    AI Auto-Commit: Base screen class.

---
+++ src/main/java/com/tcosmatic/client/gui/TCosmaticScreen.java
@@ -0,0 +1,10 @@
+package com.tcosmatic.client.gui;
+
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.text.Text;
+
+public class TCosmaticScreen extends Screen {
+    protected TCosmaticScreen(Text title) {
+        super(title);
+    }
+}
