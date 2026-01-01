8453b1594a6d70e81e30d76f9e13ac17e37d5909
Twicefear <ytd82774@gmail.com>
2026-01-01 18:34:27 +0530

    AI Auto-Commit: Cape editor screen.

---
+++ src/main/java/com/tcosmatic/client/gui/CapeEditorScreen.java
@@ -0,0 +1,9 @@
+package com.tcosmatic.client.gui;
+
+import net.minecraft.text.Text;
+
+public class CapeEditorScreen extends TCosmaticScreen {
+    public CapeEditorScreen() {
+        super(Text.literal("Cape Editor"));
+    }
+}
